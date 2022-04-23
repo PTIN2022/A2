f305cc861e509c78fe0a4093cb643b9519726af1
ShadowFurtive <silencioso4444@gmail.com>
2022-04-23 13:00:37 +0200

    23/04/2021 - Estaciones implementadas con sqlalchemy

--- cloud-service-api/app.py
+++ cloud-service-api/app.py
@@ -1,8 +1,11 @@
+import os
 from utils.db import db
+from utils.utils import fake_data
 from flask import Flask
 from routes.incidencias import incidencias
 from routes.estaciones import estaciones
-
+from models.plaza import Plaza, PlazaSchema
+from models.estacion import Estacion, EstacionSchema
 
 def init_db():
     db.init_app(app)
@@ -10,9 +13,10 @@ def init_db():
         db.create_all()
 
 
+
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"  # TODO: Pass to mysql
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"  # TODO: Pass to mysql
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # TODO: review
 app.config["TESTING"] = False
 
@@ -20,7 +24,23 @@ app.register_blueprint(incidencias)
 app.register_blueprint(estaciones)
 
 if app.config["TESTING"] is False:
+    if os.path.exists("./test.db"):
+        os.remove("./test.db")
+
     init_db()
+    with app.app_context():
+        e = Estacion("VG3", "mi casa", 720, 85, 23, 20, 130, "Alfredo_Manresa", 1300, 2000, "url")
+        db.session.add(e)
+        db.session.commit()
+
+        print(e)
+        p1 = Plaza(23,23,23,"mario", e.id)
+        p2 = Plaza(30,23,40,"mario", e.id)
+        db.session.add(p1)
+        db.session.add(p2)
+        db.session.commit()
+        
+
 
 if __name__ == "__main__":
     print("=========================================")
--- cloud-service-api/controller/estacionesController.py
+++ cloud-service-api/controller/estacionesController.py
@@ -1,136 +1,30 @@
-#from models.incidencia import Incidencia, incidencia_schema
+from models.estacion import Estacion, EstacionSchema
+from models.plaza import Plaza, PlazaSchema
 from utils.db import db
 
-estacion_list_general = [
-  {
-    "estacion": "VG1",
-    "direccion": "Av.Victor Balaguer 2",
-    "kwh_max": 720,
-    "kwh_now": 75,
-    "ocupation_max": 32,
-    "ocupation_now": 20,
-    "surface_in_meters": 130,
-    "boss": "Alfredo Manresa"
-  },
-  {
-    "estacion": "VG2",
-    "direccion": "Calle de les Empúries 2",
-    "kwh_max": 640,
-    "kwh_now": 20,
-    "ocupation_max": 32,
-    "ocupation_now": 5,
-    "surface_in_meters": 130,
-    "boss": "Pere Roca"
-  }
-]
-
-estacion_list_especificio = [
-  {
-  "estacionId": 3,
-  "estacion": "VG3",
-  "cordenadas": {
-    "latitud": 41.224115,
-    "longitud": 1.73061
-  },
-  "imagen": "http://?????????/estacion/VG3.png",
-  "kwh_max": 20000,
-  "ocupation_max": 8,
-  "ocupation_now": 4,
-  "list_plaza": [
-    {
-      "id": 1,
-      "kwh_usage": 372,
-      "battery_status": 372,
-      "battery_max": 800,
-      "cliente": "peroivi"
-    },
-    {
-      "id": 7,
-      "kwh_usage": 372,
-      "battery_status": 372,
-      "battery_max": 800,
-      "cliente": "peroivi"
-    },
-    {
-      "id": 3,
-      "kwh_usage": 372,
-      "battery_status": 372,
-      "battery_max": 800,
-      "cliente": "peroivi"
-    },
-    {
-      "id": 6,
-      "kwh_usage": 372,
-      "battery_status": 372,
-      "battery_max": 800,
-      "cliente": "peroivi"
-    }
-  ]
-  },
-  {
-  "estacionId": 4,
-  "estacion": "VG4",
-  "cordenadas": {
-    "latitud": 41.224115,
-    "longitud": 1.73061
-  },
-  "imagen": "http://?????????/estacion/VG3.png",
-  "kwh_max": 20000,
-  "ocupation_max": 8,
-  "ocupation_now": 4,
-  "list_plaza": [
-    {
-      "id": 3,
-      "kwh_usage": 372,
-      "battery_status": 372,
-      "battery_max": 800,
-      "cliente": "peroivi"
-    },
-    {
-      "id": 5,
-      "kwh_usage": 372,
-      "battery_status": 372,
-      "battery_max": 800,
-      "cliente": "peroivi"
-    },
-    {
-      "id": 7,
-      "kwh_usage": 372,
-      "battery_status": 372,
-      "battery_max": 800,
-      "cliente": "peroivi"
-    },
-    {
-      "id": 8,
-      "kwh_usage": 372,
-      "battery_status": 372,
-      "battery_max": 800,
-      "cliente": "peroivi"
-    }
-  ]
-  }
-]
-
 def get_all_estaciones():
-    return estacion_list_general
+  i = Estacion.query.all()
+  return EstacionSchema(many=True).dump(i)
 
 def get_estacion_by_id(id):
-    for estacion in estacion_list_especificio:
-        if "estacionId" in estacion:
-            if estacion["estacionId"] == int(id):
-                # arreglar esto, devuelve solo una incidencia no todas las de esa estacion
-                return estacion
+    i = Estacion.query.filter(Estacion.id == id).one_or_none()
+    if i:
+        estacion_dict = EstacionSchema().dump(i)
+
+        estacion_dict["plazas"] = []
+        for plaza in i.plazas:
+            estacion_dict["plazas"].append(PlazaSchema().dump(plaza))
+
+        return estacion_dict
+
     return None
 
+
 def delete_plaza(id, id_plaza):
-    for estacion in estacion_list_especificio:
-       if "estacionId" in estacion:
-            if estacion["estacionId"] == int(id):
-                print(estacion["list_plaza"])
-                for plaza in estacion["list_plaza"]:
-                    if "id" in plaza:
-                        if plaza["id"] == int(id_plaza):
-                            estacion["list_plaza"].remove(plaza)
-                            return True
+    i = Plaza.query.filter(Plaza.id == id_plaza).one_or_none()
+    if i:
+        db.session.delete(i)
+        db.session.commit()
+        return True
     return False
 
