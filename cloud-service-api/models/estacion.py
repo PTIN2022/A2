b61028ac38b1ecf2ca1177c376f9bc044d70753d
alvaro mdc <silencioso4444@gmail.com>
2022-04-25 11:46:44 +0200

    2022-25-04-2a tanda de arreglo de errores

--- cloud-service-api/models/estacion.py
+++ cloud-service-api/models/estacion.py
@@ -16,8 +16,8 @@ class Estacion(db.Model):
     latitud = db.Column(db.Integer, nullable=False)
     longitud = db.Column(db.Integer, nullable=False)
     imagen = db.Column(db.String(320), nullable=False)
-    plazas = db.relationship('Plaza',  backref="estacion")
- 
+    plazas = db.relationship("Plaza",  backref="estacion")
+
     def __init__(self, estacion, direccion, kwh_max, kwh_now, ocupation_max, ocupation_now, surface_in_meters, boss, latitud, longitud, imagen):
         self.estacion = estacion
         self.direccion = direccion
