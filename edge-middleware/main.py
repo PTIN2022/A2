206ce75cedd030a3c048d4bc78e3674917927674
Sergi Garcia Ibañez <sgarcia@worldsensing.com>
2022-05-02 13:04:30 +0200

    Adding cloud edge broker connection for edge middleware

--- edge-middleware/main.py
+++ edge-middleware/main.py
@@ -3,20 +3,21 @@ import json
 import paho.mqtt.client as mqtt
 
 
-client = mqtt.Client(client_id='edge1', clean_session=False)
+edge = mqtt.Client(client_id='edge2', clean_session=False)
+cloud = mqtt.Client(client_id='edge1', clean_session=False)
 
 def on_publish(client, userdata, result):
     print("Data published!")
 
 # callback despues de establecer connexion
-def on_connect(client, userdata, flags, rc):
+def on_connect_edge(client, userdata, flags, rc):
     print('(%s)' % client._client_id.decode("utf-8"), 'connected to broker')
-    client.subscribe("edge/1/#")
-    client.subscribe("estacion/200/#")
+    client.subscribe("estacion/200/#") # Faltaria que sea dinamico ?
+    client.subscribe("coche/#") # Faltaria que sea dinamico ?
         
 
 # callback del mensaje recibido
-def on_message(client, userdata, message):
+def on_message_edge(client, userdata, message):
     print('------------------------------')
     print('Data received!')
     print('topic: %s' % message.topic)
@@ -30,20 +31,41 @@ def on_message(client, userdata, message):
             if msg["type"] == "cargador":
                 print("Save Data bbdd")
                 client.publish("cloud/estacion/200", json.dumps(msg), 2)
-                
+    if message.topic == "coche":
+        if msg["type"] == "coche":
+            print("Save Data bbdd")
+            #client.publish("cloud/estacion/200", json.dumps(msg), 2)
+
+def on_message_cloud(client, userdata, message):
+    print('------------------------------')
+    print('Data received!')
+    print('topic: %s' % message.topic)
+    print('payload: %s' % message.payload.decode("utf-8"))
+    print('qos: %d' % message.qos)
+    print('------------------------------')
+
+def on_connect_cloud(client, userdata, flags, rc):
+    print('(%s)' % client._client_id.decode("utf-8"), 'connected to broker')
+    client.subscribe("edge/1/#") # Get variable entorno
 
 
 def main():
     # creamos suscriptor
 
     # definimos funciones a realizar despues de establecer connexion y recepcion de mensajes
-    client.on_connect = on_connect
-    client.on_publish = on_publish
-    client.on_message = on_message
+    edge.on_connect = on_connect_edge
+    edge.on_publish = on_publish
+    edge.on_message = on_message_edge
+
+    cloud.on_connect = on_connect_cloud
+    cloud.on_publish = on_publish
+    cloud.on_message = on_message_cloud
 
     # establecemos connexion
-    client.connect(host="test.mosquitto.org", port=1883) # BrokerCloud
-    client.loop_forever()
+    edge.connect(host="test.mosquitto.org", port=1883) # BrokerEDGE
+    cloud.connect(host="test.mosquitto.org", port=1883) # BrokerEDGE
+    edge.loop_start()
+    cloud.loop_forever()
 
 
 if __name__ == '__main__':
