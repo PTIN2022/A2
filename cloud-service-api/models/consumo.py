d7449e0f742973b42f9ed56efd675942a95979b2
ShadowFurtive <silencioso4444@gmail.com>
2022-05-14 12:42:48 +0200

    Añadido docu est

---
+++ cloud-service-api/models/consumo.py
@@ -0,0 +1,22 @@
+from utils.db import db
+from models.cargador import Cargador
+from models.horas import Horas
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+
+class Consumo(db.Model):
+    id_cargador = db.Column('id_cargador', db.ForeignKey('cargador.id_cargador'), nullable=False)
+    id_horas = db.Column('id_horas', db.ForeignKey('horas.id'), nullable=False)
+    __table_args__ = (
+        db.PrimaryKeyConstraint(id_cargador, id_horas),
+        {},
+    )
+
+    def __init__(self, id_cargador, id_horas):
+        self.id_cargador = id_cargador
+        self.id_horas = id_horas
+
+
+class ConsumoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Consumo
