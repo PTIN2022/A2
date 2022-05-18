67dff528696e1469024fd7a71b0715f84da4ddc7
ShadowFurtive <silencioso4444@gmail.com>
2022-05-18 15:49:36 +0200

    Estaciones API app acabado

--- edge-service-api/controller/estacionesController.py
+++ edge-service-api/controller/estacionesController.py
@@ -14,7 +14,7 @@ def get_estacion_by_id(id):
         estacion_dict = EstacionSchema().dump(i)
 
         estacion_dict["cargadores"] = []
-        for cargador in i.Cargadors:
+        for cargador in i.cargadores:
             estacion_dict["cargadores"].append(CargadorSchema().dump(cargador))
 
         return estacion_dict
@@ -22,10 +22,28 @@ def get_estacion_by_id(id):
     return None
 
 
-def delete_cargador(id, id_Cargador):
-    i = Cargador.query.filter(Cargador.id_cargador == id_Cargador).one_or_none()
-    if i:
-        db.session.delete(i)
-        db.session.commit()
-        return True
-    return False
+def get_estacion_by_coor(lat_str=0, long_str=0):
+    lat=float(lat_str)
+    long=float(long_str)
+    lista_estaciones = {}
+    coor = Estacion.query.with_entities(Estacion.latitud, Estacion.longitud).order_by(db.desc(Estacion.latitud)).all()
+    if coor:
+        points = []
+        lista_estaciones["Estaciones"]=[]
+        for i in range(len(coor)):
+            tupla = [coor[i][0], coor[i][1]]
+            points.append(tupla)
+        lista_order = get_ordered_list(points,lat,long)
+        valor=5
+        if len(lista_order) < 5:
+            valor = len(lista_order)
+        for i in range(valor):
+            est = Estacion.query.filter(Estacion.latitud == lista_order[i][0], Estacion.longitud == lista_order[i][1]).one_or_none()
+            est_obj = get_estacion_by_id(est.id_estacion)
+            lista_estaciones["Estaciones"].append(est_obj)
+    return lista_estaciones
+
+
+def get_ordered_list(points, x, y):
+   points.sort(key = lambda p: (p[0] - x)**2 + (p[1] - y)**2)
+   return points
--- edge-service-api/routes/estaciones.py
+++ edge-service-api/routes/estaciones.py
@@ -20,10 +20,10 @@ def get_estacion_by_id(id):
         return jsonify({"error": "Estacion not found"}), 404
 
 
-@estaciones.route('/estaciones/<id>/<id_cargador>', methods=["DELETE"])
-def delete_plaza(id, id_cargador):
-    respuesta = control.delete_cargador(id, id_cargador)
+@estaciones.route('/estaciones/coor/<lat>/<long>', methods=["GET"])
+def get_estacion_by_coor(lat, long):
+    respuesta = control.get_estacion_by_coor(lat, long)
     if respuesta:
         return jsonify(respuesta), 200
     else:
-        return jsonify({"error": "Plaza or Estacion not found"}), 404
+        return jsonify({"error": "Estacion not found"}), 404
