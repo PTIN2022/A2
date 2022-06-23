7e800be817405dd2b4f8628aeef5bd2353812b24
ShadowFurtive <silencioso4444@gmail.com>
2022-06-23 13:44:08 +0200

    login returns json-client

--- edge-service-api/routes/login.py
+++ edge-service-api/routes/login.py
@@ -12,10 +12,10 @@ def post_login():
     try:
         username = request.json["email"]
         password = request.json["password"]
-        encoded_jwt, id_cliente = control.post_login(username, password)
+        encoded_jwt, cliente = control.post_login(username, password)
 
         if encoded_jwt:
-            return jsonify({'token': encoded_jwt, 'id_cliente': id_cliente}), 200
+            return jsonify({'token': encoded_jwt, 'cliente': cliente}), 200
         else:
             return jsonify({'error': 'Invalid credentials'}), 400
     except ValueError as e:
