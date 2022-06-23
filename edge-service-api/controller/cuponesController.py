c1178f28d5ff2a426afe2e9c63eb45e2a06a5046
Sergi Garcia Ibañez <sgarcia@worldsensing.com>
2022-06-23 15:36:12 +0000

    Adding cupones

---
+++ edge-service-api/controller/cuponesController.py
@@ -0,0 +1,43 @@
+from utils.db import db
+from models.model import Cupon, CuponSchema, Cliente
+
+
+def get_cupones_user(id_user):
+    cupon_list = Cupon.query.filter(Cupon.id_cliente==id_user).all()
+    return CuponSchema(many=True).dump(cupon_list) 
+
+def add_cupon_user(cupon, id_user):
+    c = Cupon.query.filter(Cupon.cupon == cupon).one_or_none()
+    if c:
+        return {"error": "Este cupon ya existe. Usa otro nombre."}
+
+    u = Cliente.query.filter(Cliente.id_usuari == id_user)
+    if u:
+        c = Cupon(cupon, id_user)
+        db.session.add(c)
+        db.session.commit()
+        return CuponSchema().dump(c)
+    else:
+        return {"error": "Este usuario no existe"}
+            
+
+def set_cupon_estado(cupon, estado):
+    c = Cupon.query.filter(Cupon.cupon == cupon).one_or_none()
+    if c:
+        c.estado = estado 
+        db.session.commit()
+        return CuponSchema().dump(c)
+    else:
+        return {"error": "Este cupon no existe"}
+    
+
+def check_cupon_user(id_cupon, id_user):
+    cupon_list = Cupon.query.filter(Cupon.id_cliente==id_user, Cupon.cupon==id_cupon).one_or_none() 
+    if cupon_list:
+        return CuponSchema().dump(cupon_list)
+
+    return {"error": "Este usuario no tiene este cupon."}
+
+
+
+    
---
+++ edge-service-api/routes/cupones.py
@@ -0,0 +1,44 @@
+#import controller.cuponesController as control
+from flask import Blueprint, jsonify, request
+from utils.utils import token_required
+from controller.cuponesController import get_cupones_user, add_cupon_user, set_cupon_estado, check_cupon_user
+
+cupones = Blueprint('cupones', __name__)
+
+
+@cupones.route('/cupones', methods=['GET'])
+@token_required
+def get_cupones(current_usuario):
+    print(current_usuario.id_usuari)
+    return jsonify(get_cupones_user(current_usuario.id_usuari)), 200
+
+
+@cupones.route('/cupones/<id_user>', methods=['POST'])
+def post_cupones(id_user):
+    cupon = request.json["cupon"]  # Dia y hora
+    c = add_cupon_user(cupon, id_user)
+    if "error" in c:
+        return jsonify(c), 404
+
+    return jsonify(c), 200
+
+
+@cupones.route('/cupones/<id_cupon>', methods=['PUT'])
+@token_required
+def set_estado(id_cupon):
+    estado = request.json["estado"]  # Dia y hora
+    c = set_cupon_estado(id_cupon, estado)
+    if "error" in c:
+        return jsonify(c), 404
+
+    return jsonify(c), 200
+
+
+@cupones.route('/cupones/<id_cupon>', methods=['GET'])
+@token_required
+def check_cupon(current_usuario, id_cupon):
+    c = check_cupon_user(id_cupon, current_usuario.id_usuari)
+    if "error" in c:
+        return jsonify(c), 404
+
+    return jsonify(c), 200
