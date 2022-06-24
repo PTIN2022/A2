249834c135ac9578a9e1552a2cb92a2b9a550d65
ShadowFurtive <silencioso4444@gmail.com>
2022-06-24 13:41:32 +0200

    Added to show FK in worker

--- cloud-service-api/models/model.py
+++ cloud-service-api/models/model.py
@@ -245,6 +245,7 @@ class Trabajador(Usuari_t):
 class TrabajadorSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Trabajador
+        include_fk = True
         exclude = ('password',)
 
 
