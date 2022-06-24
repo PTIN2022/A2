23457cf985a812fea295039070c7ec10aac06527
ShadowFurtive <silencioso4444@gmail.com>
2022-06-24 15:09:51 +0200

    Added EP to get info worker with token

--- cloud-service-api/routes/login.py
+++ cloud-service-api/routes/login.py
@@ -1,6 +1,6 @@
 import controller.loginController as control
 from utils.utils import token_required
-
+from models.model import TrabajadorSchema
 from flask import Blueprint, jsonify, request
 
 login = Blueprint('login', __name__)
@@ -27,5 +27,14 @@ def get_logout(current_trabajador):
     if current_trabajador.cargo == "administrador" or current_trabajador.cargo == "encargado" or current_trabajador.cargo == "trabajador":
         control.get_logout(current_trabajador)
         return jsonify({'Succes': 'Logout done'}), 200
+    else:
+        return jsonify({'error': 'User not authorized'}), 400
+
+@logout.route('/token', methods=['GET'])
+@token_required
+def get_info(current_trabajador):
+    if current_trabajador.cargo == "administrador" or current_trabajador.cargo == "encargado" or current_trabajador.cargo == "trabajador":
+        respuesta = TrabajadorSchema().dump(current_trabajador)
+        return jsonify(respuesta), 200
     else:
         return jsonify({'error': 'User not authorized'}), 400
